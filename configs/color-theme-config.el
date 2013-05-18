e80aa15dfdffaa09b91ce67376b57ddd9cfa98a5
xgfone <xgfone@126.com>
2013-05-18 12:48:28 +0800

    Adjust the configure files of the plugins.
    Limit the max character length of each line of the configure files to 80.

--- configs/color-theme-config.el
+++ configs/color-theme-config.el
@@ -1,11 +1,8 @@
 ;; ----------------------------- color theme ----------------------------------
-;; 配色方案。通过M-x color-theme-select打开一个配色方案选择窗口，在配色方案上按l（小写的L）
-;; 就可以改变当前frame的配色，按i就可以改变所有frame的配色。
-;; 如果想选定一个配色方案后就一直使用它，可以按p把当前的配色方案的lisp打印出来，然后加到.emacs
-;; 文件；此时，就不需要使用(require 'color-theme)了。
-;; 注：在GNU Emacs下，有个plist-to-alist错误（在color-theme.el————本配置使用的是第6.5.0
-;;    版本————文件的261行），对于源文件，该行已经注释掉了。如在XEmacs中遇到plist错误，请取消
-;;    该注释。
+;; 配色方案。通过M-x color-theme-select打开一个配色方案选择窗口，在配色方案上
+;; 按l（小写的L）就可以改变当前frame的配色，按i就可以改变所有frame的配色。如果
+;; 想选定一个配色方案后就一直使用它，可以按p把当前的配色方案的lisp打印出来，然
+;; 后加到.emacs文件；此时，就不需要使用(require 'color-theme)了。
 
 (setq load-path (cons "~/.emacs.d/lisps/color-theme/" load-path))
 (require 'color-theme)
