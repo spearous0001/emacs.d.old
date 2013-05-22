8c158f96f1b578289a2dafd906b10a6411700576
xgfone <xgfone@126.com>
2013-05-23 00:22:15 +0800

    Add some small functions in "configs/functions.el".

--- configs/emacs-basic-config.el
+++ configs/emacs-basic-config.el
@@ -166,30 +166,6 @@
 (desktop-load-default)
 (desktop-read)
 
-;; ----------------------------------------------------------------------------
-;; ------------------------- 类似于 VIM 中的 f 移动键 -------------------------
-(defun wy-go-to-char (n char)
-    "Move forward to Nth occurence of CHAR.
-Typing `wy-go-to-char-key' again will move forwad to the next Nth
-occurence of CHAR."
-    (interactive "p\ncGo to char: ")
-    (search-forward (string char) nil nil n)
-    (while (char-equal (read-char)
-                     char)
-    (search-forward (string char) nil nil n))
-    (setq unread-command-events (list last-input-event)))
-
-
-;; ----------------------------------------------------------------------------
-;; -------------------------- 类似于 VIM 中的 % 键 ----------------------------
-(global-set-key "%" 'match-paren) 
-(defun match-paren (arg)
-    "Go to the matching paren if on a paren; otherwise insert %."
-    (interactive "p")
-    (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t (self-insert-command (or arg 1)))))
-
 
 ;; ----------------------------------------------------------------------------
 ;; ---------------------------- 快捷键自定义绑定 ------------------------------
