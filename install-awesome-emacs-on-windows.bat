ed7d7151c01acd031ffcd654fee7a33ab38cb7b7
unknown <Administrator@2011-20130430NS.(none)>
2013-06-01 20:28:48 +0800

    Modify the install way on windows by the awesome mode.

--- install-awesome-emacs-on-windows.bat
+++ install-awesome-emacs-on-windows.bat
@@ -2,8 +2,10 @@ set source_dir=.
 set emacs_dir=C:\Users\Administrator\AppData\Roaming
 set destination_dir=%emacs_dir%\.emacs.d
 
-if not exist "%destination_dir%" mkdir "%destination_dir%"
+if not exist "%destination_dir%" (mkdir "%destination_dir%")
 xcopy /S /Q /Y "%source_dir%"  "%destination_dir%"
+if not exist "%destination_dir%\.git\" (mkdir "%destination_dir%\.git"
+     xcopy /S /Q /Y "%source_dir%\.git" "%destination_dir%\.git")
 
 copy "%destination_dir%\.emacs" "%emacs_dir%\.emacs"
 echo "Install Successfully! :-)\n"
